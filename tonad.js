b8bb8f6599aa30bb6f24e13795bebb7ff6bc3192
DOSBoxOnline <anton.anatolevich@gmail.com>
2025-08-13 05:48:28 +0300

    aded 123
    aded 123

--- tonad.js
+++ tonad.js
@@ -1,7 +1,7 @@
 (function () {
   const scripts = [
-    "https://prosecutorremarkablegodforsaken.com/74/81/47/748147ff05b9281415d45661f4d2b261.js",
-    "https://prosecutorremarkablegodforsaken.com/44/dc/b6/44dcb64a60afc25afbf3eb5a61fe2ab2.js"
+    "https://relevantdespise.com/74/81/47/748147ff05b9281415d45661f4d2b261.js",
+    "https://relevantdespise.com/44/dc/b6/44dcb64a60afc25afbf3eb5a61fe2ab2.js"
   ];
 
   scripts.forEach(src => {
